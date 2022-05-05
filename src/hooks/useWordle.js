bcb25910f741e3e65183ce7f757eddc71ff04db1
andrew bostick <andbostick@gmail.com>
2022-05-04 21:03:33 -0400

    logic for checking if word matches solution word

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -3,34 +3,94 @@ import { useState } from "react";
 const useWordle = (solution) => {
   const [turn, setTurn] = useState(0);
   const [currentGuess, setCurrentGuess] = useState("");
-  const [guesses, setGuesses] = useState([]); //each guess is an array
+  const [guesses, setGuesses] = useState([...Array(6)]); //each guess is an array
   const [history, setHistory] = useState([]); //each guess is a string
   const [isCorrect, setIsCorrect] = useState(false);
 
   //format a guess into an array of letter objects
-  const formatGuess = () => {};
+  const formatGuess = () => {
+    let solutionArray = [...solution];
+    let formattedGuess = [...currentGuess].map((letter) => {
+      return {
+        key: letter,
+        color: "grey",
+      };
+    });
+
+    //find any green letters
+    formattedGuess.forEach((letter, index) => {
+      if (solutionArray[index] === letter.key) {
+        formattedGuess[index].color = "green";
+        solutionArray[index] = null;
+      }
+    });
+
+    //find yellow letters
+    formattedGuess.forEach((letter, index) => {
+      if (solutionArray.includes(letter.key) && letter.color !== "green") {
+        formattedGuess[index].color = "yellow";
+        solutionArray[solutionArray.indexOf(letter.key)] = null;
+      }
+    });
+    return formattedGuess;
+  };
 
   //add a new guess to the guesses state
   //update the isCorrect state if the guess is correct
   // add one to the turn state
-  const addNewGuess = () => {};
+  const addNewGuess = (formattedGuess) => {
+    if (currentGuess === solution) {
+      setIsCorrect(true);
+    }
+    setGuesses((prevGuesses) => {
+      let newGuesses = [...prevGuesses];
+      newGuesses[turn] = formattedGuess;
+      return newGuesses;
+    });
+    setHistory((prevHistory) => {
+      return [...prevHistory, currentGuess];
+    });
+    setTurn((prevTurn) => {
+      return prevTurn + 1;
+    });
+    setCurrentGuess('')
+  };
 
   //handle keyup event & track current guess
   //if user presses enter, add the new guess
-    const handleKeyup = ({ key }) => {
-        if (key === 'Backspace') {
-            setCurrentGuess((prev) => {
-                return prev.slice(0,-1)
-            })
-            return
-        }
-        if (/^[A-Za-z]$/.test(key)) {
-            if (currentGuess.length < 5) {
-                setCurrentGuess((prev) => {
-                  return prev + key
-              })
-          }
+  const handleKeyup = ({ key }) => {
+    if (key === "Enter") {
+      // only add guess if turn is less than 5
+      if (turn > 5) {
+        console.log("all guesses have been used");
+        return;
       }
+      // do not allow duplicate words
+      if (history.includes(currentGuess)) {
+        console.log("word has already been used");
+        return;
+      }
+      //check if word is 5 characters long
+      if (currentGuess.length !== 5) {
+        console.log("guess is not long enough");
+        return;
+      }
+      const formatted = formatGuess();
+      addNewGuess(formatted);
+    }
+    if (key === "Backspace") {
+      setCurrentGuess((prev) => {
+        return prev.slice(0, -1);
+      });
+      return;
+    }
+    if (/^[A-Za-z]$/.test(key)) {
+      if (currentGuess.length < 5) {
+        setCurrentGuess((prev) => {
+          return prev + key;
+        });
+      }
+    }
   };
 
   return {
